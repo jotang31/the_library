c7e246f71dd76336f77a45947858d3c948ef8541
jotang31 <60412677+jotang31@users.noreply.github.com>
2024-03-26 00:27:53 -0700

    Update Library.h

--- Library.h
+++ Library.h
@@ -4,13 +4,16 @@
 #include <string>
 #include <map>
 
+/**
+ * An inventory system to track and update the status of books in a library. 
+ **/
 class Library
 {
 public:
     void printInventory() const;
     void addBook(std::string nameOfBook);
     void editBookStatus(std::string nameOfBook);
-
+    
 private:
     std::map<std::string, int> books; 
         // first (key) is book name. 
@@ -18,7 +21,6 @@ private:
             // 1 -> checked out
             // 2 -> on shelf
             // 3 -> missing
-    
 };
 
 #endif
