fea542d2bc334838ee32209dd4b049c647e266a1
jotang31 <60412677+jotang31@users.noreply.github.com>
2024-03-26 00:29:08 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,37 +1,43 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <iostream>
 #include <iomanip>
+
 #include "Library.h"
+
 using namespace std;
+
 int main()
 {
-    cout << "\\\\\\\\\\ Library Inventory Management System /////\n\n" 
-        << "Please select an option: "
+    Library testLibrary;
+    
+    cout << "\\\\\\\\\\ Library Inventory Management System /////\n\n";
+    
+    bool done = false;
+    while(!done)
+    {
+        cout << "Please select an option: \n"
         << "1. View inventory\n"
         << "2. Add book to inventory\n"
         << "3. Edit book status\n"
+        << "4. Exit"
         << endl;
     
-    int input = 0'
-    cin >> input;
-    
-    switch(input)
-    {
-        case 1: 
+        int input = 0;
+        cin >> input;
+        cin.ignore();
+        
+        switch(input)
+        {
+            case 1: testLibrary.printInventory(); break;
+            case 2: testLibrary.addBook("hello"); break;
+            case 3: testLibrary.editBookStatus("hello"); break;
+            case 4: done = true; break;
+            default: cerr << "Invalid selection";
+        }
     }
     
     
-    cout << setw(23) << left << "Hello World";
-    cout << setw(20) << right << "on shelf" << endl;
-    
-    
+    cout << "\nExiting Library Inventory Management System...\n\n"
+        << "\\\\\\\\\\ Thank you for using the library /////" << endl;
     
     
     return 0;
